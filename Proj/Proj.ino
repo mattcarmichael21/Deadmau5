3a72b465b0e1433fd9ccf2d58bdce668c54fe907
Anthony <azulua2@uic.edu>
2015-11-23 15:36:11 -0600

    Added different functions for the eyes Made use of the TrueRandom library

--- Proj/Proj.ino
+++ Proj/Proj.ino
@@ -5,87 +5,120 @@
 // Program for Arduino for LED Circles
 
 #include "Adafruit_NeoPixel.h"
+#include <TrueRandom.h>
  
 #define PIN 6
+#define PIN2 5
 #define NUM_PIXELS 24
  
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
- 
+Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(NUM_PIXELS, PIN2, NEO_GRB + NEO_KHZ800);
+int rNum;       // r pixel value 0 - 255 
+int gNum;       // g pixel value 0 - 255 
+int bNum;       // b pixel value 0 - 255
+
 void setup()
 {
   pixels.begin();
+  pixels2.begin();
 }
  
 void loop()
 {
   
   //threeColor();
-  yellowLight();
-  pixels.clear();
+  //light();
+  //flash();
+  randomLight();
+  delay(350);
+  clearAll();
+  delay(350);
+
 }
 
-void blueLight()
+void blue()
 {
+  delay(1000);
   for(int i=0; i<NUM_PIXELS; i++)
   {
-    //pixels.clear();
-    pixels.setPixelColor(i, 0, 0, 255); 
+    pixels.setPixelColor(i, pixels.Color(0, 0, 255));
     pixels.show();
-    delay(100);   
+    pixels2.setPixelColor(i, pixels2.Color(0, 0, 255));
+    pixels2.show();   
   }
 }
 
-void redLight()
+void green()
 {
+  delay(1000);
   for(int i=0; i<NUM_PIXELS; i++)
   {
-    //pixels.clear();
-    pixels.setPixelColor(i, 255, 0, 0); 
+    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
     pixels.show();
-    delay(100);   
+    pixels2.setPixelColor(i, pixels2.Color(0, 255, 0));
+    pixels2.show();   
   }
 }
 
-void greenLight()
+void red()
 {
+  delay(1000);
   for(int i=0; i<NUM_PIXELS; i++)
   {
-    //pixels.clear();
-    pixels.setPixelColor(i, 0, 255, 0); 
+    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
     pixels.show();
-    delay(100);   
+    pixels2.setPixelColor(i, pixels2.Color(255, 0, 0));
+    pixels2.show();   
   }
 }
+
+
 
 void threeColor()
 {
   for(int i = 0; i < NUM_PIXELS; i++)
   {
     pixels.clear();
+    pixels2.clear();
     pixels.setPixelColor(i, pixels.Color(0,255,0));
+    pixels2.setPixelColor(i, pixels2.Color(0,255,0));
     pixels.setPixelColor(23-i, pixels.Color(255,0,0));
+    pixels2.setPixelColor(23-i, pixels2.Color(255,0,0));
     if (i<12)
     {
       pixels.setPixelColor(12+i, pixels.Color(0,0,255));
+      pixels2.setPixelColor(12+i, pixels2.Color(0,0,255));
     } else
     {
       pixels.setPixelColor(i-12, pixels.Color(0,0,255));
+      pixels2.setPixelColor(i-12, pixels2.Color(0,0,255));
     }
     pixels.show();
+    pixels2.show();
     delay(50);
   }
 }
 
-void yellowLight()
+void randomLight()
 {
+  delay(1000);
+  rNum = (TrueRandom.random(1,255));            // store random number from 1 to 255 into r value
+  gNum = (TrueRandom.random(1,255));            // store random number from 1 to 255 into g value
+  bNum = (TrueRandom.random(1,255));            // store random number from 1 to 255 into b value
+  
   for(int i=0; i<NUM_PIXELS; i++)
   {
-    //pixels.clear();
-    pixels.setPixelColor(i, pixels.Color((255-(i*10)), 200, (255-(i*10))));
+    pixels.setPixelColor(i, pixels.Color(rNum, gNum, bNum));
     pixels.show();
-    delay(100);   
+    pixels2.setPixelColor(i, pixels2.Color(rNum, gNum, bNum));
+    pixels2.show();   
   }
 }
 
+void clearAll()
+{
+  pixels.clear();
+  pixels2.clear();
+}
 
 
